27d3d099db7c69db9188792607eaec8bd4995a75
Joe Shiels <joeshiels@Joes-MacBook-Pro.local>
2016-06-03 23:20:00 +0100

    Fixed startup bug

---
+++ server.js
@@ -0,0 +1,13 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+	console.log(socket.handshake.address);
+});
