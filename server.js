12d3d028e48a347638f5b5a89ac692fdceffa9f6
Joe Shiels <joeshiels@Joes-MacBook-Pro.local>
2016-06-03 23:32:42 +0100

    Fixed port issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000);
+server.listen(process.env.port || 3000);
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
